3ad619168a19024aa1e357f734ed3b7cc5304d84
manuel-morales-orbidi <manuel.morales@orbidi.com>
2025-02-19 09:55:55 -0500

    init methods

---
+++ src/practicas/Persona.java
@@ -0,0 +1,14 @@
+package practicas;
+
+public class Persona {
+    int id;
+    String cedula;
+    String nombre;
+    String apellido;
+    String direccion;
+    String telefono;
+
+    public Persona() {
+
+    }
+}
