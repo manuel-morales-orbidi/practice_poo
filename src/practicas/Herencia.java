7b51a49c7b0a510eba6bcb7e4d9ab9c0737e614b
Luis Gamboa <luis.gamboa@orbidi.com>
2025-02-27 10:40:58 -0500

    Agregando nueva clase Proveedor con atributos heredados de personas_Herencia use atributos heredados de Proveedor

--- src/practicas/Herencia.java
+++ src/practicas/Herencia.java
@@ -5,8 +5,14 @@ public class Herencia {
         Empleado empleado = new Empleado();
         Consultor consultor = new Consultor();
         Persona persona = new Persona();
+        Proveedor proveedor = new Proveedor();
 
         empleado.getNumero_empleado();
         persona.getTelefono();
+        proveedor.getIdentificador_Proveedor();
+        proveedor.getNumero_Proveedor();
+        proveedor.getTelefono_Proveedor();
+
+
     }
 }
